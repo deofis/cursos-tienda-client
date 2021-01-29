88afbca14cb4a0a6a852c25b7f29fe9fb018499c
lucasutnfrc <lucasmateo68@gmail.com>
2021-01-29 13:33:02 -0300

    servicios

--- src/app/admin-options/producto.service.ts
+++ src/app/admin-options/producto.service.ts
@@ -13,7 +13,9 @@ import { Promocion } from './admin-promos/clases/promocion';
   providedIn: 'root'
 })
 export class ProductoService {
-url:string=API_BASE_URL+"/api";
+
+  url:string=API_BASE_URL+"/api";
+
   constructor(private http: HttpClient) { }
 
   createNewProduct(producto:Producto):Observable<Producto>{
@@ -21,62 +23,64 @@ url:string=API_BASE_URL+"/api";
     return this.http.post(`${this.url}/productos`,producto).pipe(
       map((response:any) =>response.producto as Producto)
     )
-  }
+  };
 
   
   getAllProperties():Observable<PropiedadProducto[]>{
     return this.http.get(`${this.url}/productos/propiedades`).pipe( map( response => response as PropiedadProducto[]));
   
-  }
+  };
+
   getPropertiesOfSubcategory(subcategoriaId:number):Observable<PropiedadProducto[]>{
     return this.http.get(`${this.url}/subcategorias/${subcategoriaId}/propiedades`).pipe( map( (response:any)=> response.propiedades as PropiedadProducto[]));
   
-  }
+  };
+
   getPropertiesOfAProduct(id:number):Observable<PropiedadProducto[]>{
     return this.http.get(`${this.url}/productos/${id}/propiedades`).pipe( map( (response:any)=> response.propiedades as PropiedadProducto[]));
-  }
+  };
+
   createNewSku(newSku:Sku, productoId:number):Observable<Sku>{
     return this.http.post(`${this.url}/productos/${productoId}/skus`,newSku).pipe(
       map((response:any) =>response )
     )
-  }
+  };
+
   getAllTheSkus(productoId:number):Observable<Sku[]>{{
     return this.http.get(`${this.url}/productos/${productoId}/skus`).pipe(
       map((response:any) =>response.skus as Sku[] )
     )
-  }}
+  }};
+
   getSku(productoId:number, skuId:number){
     return this.http.get(`${this.url}/productos/${productoId}/skus/${skuId}`).pipe(
       map((response:any) =>response.sku as Sku )
     )
-  }
+  };
+
   generateSkus(productoId:number){
     return this.http.post(`${this.url}/productos/${productoId}/generarSkus`,null).pipe(
       map((response:any) =>console.log(response) )
     );
-  }
+  };
 
   deleteSku(skuId:number){
     return this.http.delete(`${this.url}/productos/skus/${skuId}`)
-  }
+  };
 
   editarPrecioSku(idSku:number, precio){
     let parametros=new HttpParams();
     parametros=parametros.append("precio",precio);
     return this.http.put(`${this.url}/productos/skus/${idSku}/precios/base`,null, {params:parametros});
 
-  }
+  };
 
   editarDisponibilidadSku(idSku:number, disponibilidad){
     let parametros=new HttpParams();
     parametros=parametros.append("disponibilidad",disponibilidad);
     return this.http.put(`${this.url}/productos/skus/${idSku}/disponibilidad`,null, {params:parametros});
 
-  }
-  // editarSku( skuEditado){
-  //   return this.http.put(`${this.url}/productos/skus/${skuEditado.id}`,null,skuEditado);
-
-  // }
+  };
 
   uploadPhoto(archivo: File, id: any){
     let formData = new FormData();
@@ -85,7 +89,8 @@ url:string=API_BASE_URL+"/api";
     return this.http.post(`${this.url}/productos/${id}/fotos/principal`,formData).pipe(
       map((response:any) =>response)
     )
-  }
+  };
+
   crearNewPromotionSub(promocion:Promocion, id:number){
 
     return this.http.post(`${this.url}/subcategorias/${id}/promociones`, promocion);
@@ -112,7 +117,7 @@ url:string=API_BASE_URL+"/api";
     return this.http.put(`${this.url}/productos/${producto.id}`, producto).pipe(map((resp:any) => {
       return resp
     }))
-  }
+  };
 
   actualizarDisponibilidadProducto(idProducto: number, disponibilidad: any){
     let parametros = new HttpParams();
@@ -120,7 +125,7 @@ url:string=API_BASE_URL+"/api";
     return this.http.put(`${this.url}/productos/${idProducto}/disponibilidad`, null, {params: parametros}).pipe(map((resp:any) => {
       return resp
     }))
-  }
+  };
 
   actualizarPrecioBaseProducto(idProducto: number, precio: any){
     let parametros = new HttpParams();
@@ -128,13 +133,13 @@ url:string=API_BASE_URL+"/api";
     return this.http.put(`${this.url}/productos/${idProducto}/precios/base`, null, {params: parametros}).pipe(map((resp:any) => {
       return resp
     }))
-  }
+  };
 
   actualizarDatosSku(sku: Sku){
     return this.http.put(`${this.url}/productos/skus/${sku.id}`, sku).pipe(map((resp:any) => {
       return resp;
     }))
-  }
+  };
 
  
 
